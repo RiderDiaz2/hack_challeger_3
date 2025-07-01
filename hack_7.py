a9c4bda5eac3e235dbca795ad54ad1c495905cb2
RiderDiaz2 <rideralbertodiaz1210@gmail.com>
2025-07-01 16:44:56 -0400

    feat(update) hack_python_1

--- hack_7.py
+++ hack_7.py
@@ -4,5 +4,10 @@ loop: while [] output => [5,4,3,2,1,0]
 
 def fn_hack_7():
     result = []
-    #...
+    num = 5
+    while num >= 0:
+        result.append(num)
+        num -= 1
     return result  
+
+print(fn_hack_7)
