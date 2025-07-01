91298fbf3c15c9cc3ed506d11fe5b56aa22b78f0
RiderDiaz2 <rideralbertodiaz1210@gmail.com>
2025-07-01 16:44:30 -0400

    feat(update) hack_python_1

--- hack_9.py
+++ hack_9.py
@@ -3,6 +3,13 @@ loop: while [1,2,3] ouput => [1,'@',2,'@',3,'@']
 """
 
 def fn_hack_9():
-    result = [1,2,3]
-    #...
-    return result  
+    original_list = [1, 2, 3] 
+    result = []               
+    i = 0                   
+    while i < len(original_list): 
+        result.append(original_list[i]) 
+        result.append('@')            
+        i += 1                         
+    return result 
+
+print(fn_hack_9)
